cfd77e54908c914e0f68eb03e81f45b1e782317d
Sean Coleman <scoleman2296@gmail.com>
2022-08-22 01:21:18 -0400

    Connecting the JS and rendering HTML

--- Dog.js
+++ Dog.js
@@ -1,1 +1,19 @@
 // Create the Dog class here
+
+class Dog {
+    constructor(data) {
+        Object.assign(this, data)
+    }
+
+    getDogHtml() {
+        const { name, avatar, age, bio } = this
+        return `
+        <img class="dog-image" src="${avatar}">
+        <div class="description">
+            <h2>${name}, ${age}</h2>
+            <p>${bio}</p>
+        </div>`
+    }
+}
+
+export default Dog
--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Remember to import the data and Dog class!
+
+import dogs from '/data.js'
+import Dog from '/Dog.js'
+
+function render() {
+    document.getElementById("main").innerHTML = dog.getDogHtml()
+}
+
+let dog = new Dog(dogs[1])
+render()
