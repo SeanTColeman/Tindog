ef1119c0417d3aa3ba728e8ce8627c1fa9af1eb7
Sean Coleman <scoleman2296@gmail.com>
2022-08-29 01:44:14 -0400

    Completing the functionality and design

--- index.js
+++ index.js
@@ -6,7 +6,9 @@ import Dog from '/Dog.js'
 const dislikeBtn = document.getElementById('dislike-btn')
 const likeBtn = document.getElementById('like-btn')
 const choiceBtns = document.getElementById('choice-btns')
+let currentDogIndex = 0
 let waitingTurn = false
+let dog = new Dog(dogs[currentDogIndex])
 
 choiceBtns.addEventListener('click', e => {
     if (!waitingTurn) {
@@ -22,20 +24,36 @@ choiceBtns.addEventListener('click', e => {
 
 function nextDog() {
     waitingTurn = true
+
+    if (dog.hasBeenLiked) {
+        dogs.splice(currentDogIndex, 1)
+    }
+
+    currentDogIndex++
+    if (currentDogIndex > dogs.length - 1) {
+        currentDogIndex = 0
+    }
+
     setTimeout(() => {
-        dog = new Dog(dogs[randomDog()])
-        render()
-        waitingTurn = false
-    }, "1000") 
+        if (dogs.length === 0) {
+            document.getElementById("main").innerHTML = resetMsg
+        } else {
+            dog = new Dog(dogs[currentDogIndex])
+            render()
+            waitingTurn = false
+        }
+
+    }, "1000")
+    console.log(dogs.length)
 }
+
+const resetMsg = `
+    <div class="reset-msg">
+        There are no more dogs in your area. Please check back later.
+    </div>`
 
 function render() {
     document.getElementById("main").innerHTML = dog.getDogHtml()
 }
 
-function randomDog() {
-    return Math.floor(Math.random() * dogs.length)
-}
-
-let dog = new Dog(dogs[randomDog()])
 render()
